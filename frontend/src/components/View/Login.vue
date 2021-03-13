08678c424885ea2c4b304e442c6075b3c8fe9e36
Juan Fernandez <juan@Juans-MacBook-Pro.local>
2021-03-13 20:05:23 +0100

    fix textfields input for login

--- frontend/src/components/View/Login.vue
+++ frontend/src/components/View/Login.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <input type="text" v-model="username" placeholder="Username" size="42">
-    <br><br>
-    <input type="password" v-model="password" placeholder="Password" size="42">
+    <input type="text" v-model="username" placeholder="Username" style="width: 300px;">
     <br>
-    <router-link to="/profile" tag="button">Log in</router-link>
+    <div class="descriptionarea">
+      <input type="password" v-model="password" placeholder="Password" style="width: 300px;">
+      <br>
+      <span class="buttonfortxtarea">
+        <router-link to="/profile" tag="button" class="button button1">Log in</router-link>
+      </span>
+    </div>
   </div>
 </template>
+
+<style scoped>
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+</style>
