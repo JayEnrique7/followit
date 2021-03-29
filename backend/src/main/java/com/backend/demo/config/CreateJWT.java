aa2184eb4d841b1840d197472070155bcda3328c
Juan Fernandez <juan@Juans-MacBook-Pro.local>
2021-03-29 15:58:57 +0200

    build the token

--- backend/src/main/java/com/backend/demo/config/CreateJWT.java
+++ backend/src/main/java/com/backend/demo/config/CreateJWT.java
@@ -1,11 +1,10 @@
 package com.backend.demo.config;
 
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
-import java.security.Key;
+import java.nio.charset.StandardCharsets;
 
-import io.jsonwebtoken.*;
-
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,39 +14,28 @@ import io.jsonwebtoken.Jwts;
 public class CreateJWT {
     private static final String SECRET_KEY = "oeRaYY7Wo24sDqKSX3IM9ASGmdGPmkTd9jo1QTy4b7P9Ze5_9hKolVX8xNrQDcNRfVEdTZNOuOyqEGhXEbdJI-ZQ19k_o9MI0y3eZN2lp9jow55FfXMiINEdt1XR85VipRLSOkT6kSpzs2x-jbLDiz9iFVzkd81YKxMgPA7VfZeQUm4n-mOmnWMaVX30zGFU4L3oPBctYKkl4dYfqYWqRNfrgPJVi5DGFjywgxx0ASEiJHtV72paI3fDR2XwlSkyhhmY-ICjCRmsJN4fX1pdoL8a18-aQrvyu4j0Os6dVPYIoPvvY0SAZtWYKHfM15g7A3HD4cVREf9cUsprCRK93w";
 
-    public String createJWT(int id, String issuer, String subject, long ttlMillis) {
+    public String createJWT(String id, boolean admin, String name, String subject, int minutes) {
 
-        //The JWT signature algorithm we will be using to sign the token
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-
-        long nowMillis = System.currentTimeMillis();
-        Date now = new Date(nowMillis);
-
-        //We will sign our JWT with our ApiKey secret
-        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
-        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
-
-        //Let's set the JWT Claims
-        JwtBuilder builder = Jwts.builder().setId(Integer.toString(id))
-                .setIssuedAt(now)
+        return Jwts.builder()
                 .setSubject(subject)
-                .setIssuer(issuer)
-                .signWith(signatureAlgorithm, signingKey);
+                .setId(id)
+                .setIssuedAt(addMinutes(0))
+                .setExpiration(addMinutes(minutes))
+                .claim("name", name)
+                .claim("admin", admin)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY.getBytes(StandardCharsets.UTF_8))
+                .compact();
+    }
 
-        //if it has been specified, let's add the expiration
-        if (ttlMillis > 0) {
-            long expMillis = nowMillis + ttlMillis;
-            Date exp = new Date(expMillis);
-            builder.setExpiration(exp);
-        }
-
-        //Builds the JWT and serializes it to a compact, URL-safe string
-        return builder.compact();
+    private Date addMinutes(int minutes) {
+        Calendar now = Calendar.getInstance();
+        now.add(Calendar.MINUTE, minutes);
+        return now.getTime();
     }
 
     private Claims decodeJWT(String jwt) {
         return Jwts.parser()
-                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .setSigningKey(SECRET_KEY.getBytes(StandardCharsets.UTF_8))
                 .parseClaimsJws(jwt).getBody();
     }
 
--- backend/src/test/java/com/backend/demo/config/CreateJWTTest.java
+++ backend/src/test/java/com/backend/demo/config/CreateJWTTest.java
@@ -1,9 +1,5 @@
 package com.backend.demo.config;
 
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,13 +7,11 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class CreateJWTTest {
@@ -30,35 +24,41 @@ public class CreateJWTTest {
     }
 
     @Test
-    public void hejhej() {
-        String iss = "issuer";
-        String sub = "subject";
+    public void createJWT_Test() {
+        String id = UUID.randomUUID().toString();
+        String sub = "quagmire@mail.com";
+        String name = "Quagmire";
+        Date iat = new Date();
+        Date exp = new Date();
+        int randomMinutes = (int) (Math.random() * (59 - 1)) + 1;
 
-        System.out.println(System.currentTimeMillis());
+        String jwtToken = createJWT.createJWT(id, false, name, sub, randomMinutes);
 
-        String he = createJWT.createJWT(1, iss, sub, 1616959266350123L);
-        createJWT.getMapFromIoJsonWebTokenClaims(he).forEach(
+        createJWT.getMapFromIoJsonWebTokenClaims(jwtToken).forEach(
                 (key, value) -> {
                     if (key.equalsIgnoreCase("sub")) {
                         assertEquals(sub, value);
                     }
-                    if (key.equalsIgnoreCase("iss")) {
-                        assertEquals(iss, value);
-                    }
                     if (key.equalsIgnoreCase("jti")) {
-                        assertEquals("1", value);
+                        assertEquals(id, value);
                     }
-                    if (key.equalsIgnoreCase("exp") || key.equalsIgnoreCase("iat")) {
-
-
-                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
-                        Date date = new Date(Long.parseLong(value.toString()));
-                        System.out.println(key + " " + formatter.format(date));
+                    if (key.equalsIgnoreCase("name")) {
+                        assertEquals(name, value);
                     }
-
+                    if (key.equalsIgnoreCase("admin")) {
+                        assertFalse(Boolean.parseBoolean(value.toString()));
+                    }
+                    if(key.equalsIgnoreCase("iat")) {
+                        iat.setTime(Long.parseLong(value.toString())*1000);
+                    }
+                    if (key.equalsIgnoreCase("exp")) {
+                        exp.setTime(Long.parseLong(value.toString())*1000);
+                    }
                 });
 
+        long diffInMillis = Math.abs(exp.getTime() - iat.getTime());
+        int diff = (int) TimeUnit.MINUTES.convert(diffInMillis, TimeUnit.MILLISECONDS);
+
+        assertEquals(randomMinutes, diff);
     }
-
-
 }
