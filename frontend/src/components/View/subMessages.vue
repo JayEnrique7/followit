f2542b0cfd8da0ec8993f76a1958f71efd0cbf42
Juan Fernandez <juan.fernandez@programmer.net>
2021-04-12 23:23:51 +0200

    add MessagesPostResponse.java and remove Role.java

---
+++ frontend/src/components/View/subMessages.vue
@@ -0,0 +1,18 @@
+<template>
+        <div class="message">
+            <strong>Lars Olof</strong>
+            <br>
+            <br>
+            I like what I see!
+        </div>
+</template>
+
+<style scoped>
+.message {
+  padding: 20px;
+  background-color: #4267B2;
+  color: white;
+  border-radius: 5px;
+  text-align: initial;
+}
+</style>
